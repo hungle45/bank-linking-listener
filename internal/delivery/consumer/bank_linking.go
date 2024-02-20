fe56f28f2d634375213d5f49ba6cda028b29e2d6
hungln <hungln.work@gmail.com>
2024-02-20 14:38:08 +0700

    fix infinite retry consumer when bank not found

--- internal/delivery/consumer/bank_linking.go
+++ internal/delivery/consumer/bank_linking.go
@@ -3,6 +3,7 @@ package consumer
 import (
 	"context"
 	"demo/bank-linking-listener/internal/delivery/consumer/consumer_dto"
+	"demo/bank-linking-listener/pkg/errorx"
 	"encoding/json"
 	"errors"
 	"log"
@@ -14,15 +15,19 @@ func (c *Controller) HandleBankLinking(ctx context.Context, message *sarama.Cons
 	var req consumer_dto.BankLinkRequest
 	err := json.Unmarshal(message.Value, &req)
 	if err != nil {
-		return errors.New("failed to unmarshal message")
+		log.Print("[HandleBankLinking] Failed to unmarshal message")
+		return nil
 	}
 
 	err = c.bankService.LinkBank(ctx, req.UserID, req.BankCode)
 	if err != nil {
-		log.Printf("Failed to link bank for user %v: %s", req.UserID, err)
-		return errors.New(err.Error())
+		log.Printf("[HandleBankLinking] Failed to link bank %v for user %v: %s", req.BankCode, req.UserID, err)
+		if err == errorx.ErrorInternal {
+			return errors.New(err.Error())
+		}
+		return nil
 	}
 
-	log.Printf("Bank %s linked for user %v", req.BankCode, req.UserID)
+	log.Printf("[HandleBankLinking] Bank %s linked for user %v", req.BankCode, req.UserID)
 	return nil
 }
